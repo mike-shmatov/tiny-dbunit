1c57523c04ba706345cb88788ec69f4c6762a136
mike <shmatov@inbox.ru>
2017-09-15 13:09:30 +0300

    small refactorings

--- src/AbstractDbUnitTestCase.php
+++ src/AbstractDbUnitTestCase.php
@@ -3,18 +3,18 @@ namespace Tiny\DbUnit;
 
 abstract class AbstractDbUnitTestCase extends \PHPUnit_Extensions_Database_TestCase implements Interfaces\ConnectorsFactory
 {
-    private $connectorsFactory;
+    private static $connectorsFactory;
     private static $connector;
     protected $pdo; // can be used in real TestCase, for example as PDO for gateway construction
     protected static $defaultDbConnection;
     
     public function __construct($name = null, array $data = array(), $dataName = '') {
         parent::__construct($name, $data, $dataName);
-        $this->connectorsFactory = new DbConnectorsFactory();
+        self::$connectorsFactory = new DbConnectorsFactory();
     }
     
     public function setUp(){
-        $this->pdo = self::$connector->getPDO();
+        $this->fillPdo();
         parent::setUp();
     }
     
@@ -28,7 +28,12 @@ abstract class AbstractDbUnitTestCase extends \PHPUnit_Extensions_Database_TestC
 
     public function makeInMemoryConnector() {
         if(is_null(self::$connector)){
-            self::$connector = $this->connectorsFactory->makeInMemoryConnector();
+            self::$connector = self::$connectorsFactory->makeInMemoryConnector();
         }
+        $this->fillPdo();
     }
+    
+        private function fillPdo(){
+            $this->pdo = self::$connector->getPDO();
+        }
 }
